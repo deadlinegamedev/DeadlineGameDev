e164560334a92ae2dc69e58ad5e8233bf3d9fb7d
deadlinegamedev <info@deadlinegamedev.com>
2014-12-19 23:59:38 +0100

    added development project

---
+++ Ide/scripts/index.js
@@ -0,0 +1,35 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+})();
+
+
+angular.module('deadlinegamedev', ['ionic'])
+
+    .controller('GameCtrl', ['$scope', '$sce', function ($scope, $sce) {
+
+        //$scope.frameUrl = $sce.trustAsResourceUrl('http://deadlinegamedev.github.io/deadlinegamedev/game' + $scope.game.id + '/index.html');
+        $scope.frameUrl = $sce.trustAsResourceUrl('game/index.html');
+
+    }]);
